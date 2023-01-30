309e220406fad762ff88b3801741d2089697d464
Osrosario <114501083+Osrosario@users.noreply.github.com>
2023-01-30 15:08:22 -0500

    Completed Demo

--- app/src/main/java/edu/quinnipiac/ser210/beeradvisor/MainActivity.kt
+++ app/src/main/java/edu/quinnipiac/ser210/beeradvisor/MainActivity.kt
@@ -2,10 +2,40 @@ package edu.quinnipiac.ser210.beeradvisor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val findBeer = findViewById<Button>(R.id.find_beer)
+
+        fun getBeers(color: String): List<String>
+        {
+            return when (color)
+            {
+                "Light" -> listOf("Jail Pale Ale", "Lager Lite")
+                "Amber" -> listOf("Jack Amber", "Red Moose")
+                "Brown" -> listOf("Brown Bear Beer", "Bock Browie")
+                else -> listOf("Gout Stout", "Dark Daniel")
+            }
+        }
+
+        findBeer.setOnClickListener()
+        {
+            val beerColor = findViewById<Spinner>(R.id.beer_color)
+            val color = beerColor.selectedItem.toString()
+            val beerList = getBeers(color)
+            val beers = beerList.reduce { str, item -> str + '\n' + item}
+            val brands = findViewById<TextView>(R.id.brands)
+            brands.text = beers
+        }
+
+
     }
 }
